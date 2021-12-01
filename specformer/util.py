1d0b10950976d9332a52eb92ce5c3500d7cb2b25
kavorite <iseurie@gmail.com>
2021-12-01 18:12:31 -0500

    add resizing function for spectrograms

--- specformer/util.py
+++ specformer/util.py
@@ -20,7 +20,7 @@ def apply_rotary_embedding(x):
         x = jnp.stack((-x2, x1), axis=-1)
         return x.reshape(*x.shape[:-2], -1)
 
-    x = x[..., : x.shape[-2] % -4, :]
+    x = x[..., : x.shape[-2] % -4]
     sin, cos = fixed_pos_embedding(x)
     sin, cos = map(partial(jnp.repeat, repeats=2, axis=-1), (sin, cos))
     return (x * cos) + (rotate_every_two(x) * sin)
@@ -30,13 +30,26 @@ def strip_pool(r, x):
     return sum(x[..., i::r] for i in range(r)) / r
 
 
+def resize(a, d):
+    s_h, s_w = a.shape[-2:]
+    d_h, d_w = d
+    ys = jnp.linspace(0, s_h, d_h, endpoint=False)
+    xs = jnp.linspace(0, s_w, d_w, endpoint=False)
+    cd = jnp.meshgrid(ys, xs)
+    a = jax.scipy.ndimage.map_coordinates(s, cd, order=1)
+    ax = list(a.shape[:-2]) + [a.ndim - 1, a.ndim - 2]
+    return jnp.transpose(a, axes=ax)
+
+
 def spectrogram(a, n_fft=128, window=jnp.hanning, eps=1e-12):
     n = n_fft
     rpad = -a.shape[-1] % n
     wins = jnp.pad(a, (0, rpad)).reshape(-1, n) * window(n)
     fftc = jnp.fft.fftshift(jnp.fft.fft(wins, n=n))[..., n // 2 : n]
     fftr = jnp.real(fftc * jnp.conj(fftc))
-    return jnp.log(jnp.maximum(fftr, eps))
+    fftr = jnp.log(jnp.maximum(fftr, eps))
+    axes = list(fftr.shape[:-2]) + [fftr.ndim - 1, fftr.ndim - 2]
+    return jnp.transpose(fftr, axes=axes)
 
 
 def ws_getter(next_getter, value, context):
